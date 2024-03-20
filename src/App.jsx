6c49938861155a4a88123dd64eece011dadb945b
HarishPraK <rjaharrymicro1212@gmail.com>
2024-03-20 19:31:47 +0530

    feat:react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
@@ -17,11 +17,16 @@ function App() {
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/order" element={<PlaceOrder />} /> */}
+
+          <Route path="/vite-react-router/" element={<Home />} />
+          <Route path="/vite-react-router/cart" element={<Cart />} />
+          <Route path="/vite-react-router/order" element={<PlaceOrder />} />
         </Routes>
       </div>
+      <Outlet />
       <Footer />
     </>
   );
